8a88c07972ef593a1d92704dec24ae887436d1c0
thepigcat <benpospo@gmail.com>
2024-01-08 19:06:51 +0100

    test

--- src/main/java/com/pigrat/stoleurdata/Main.java
+++ src/main/java/com/pigrat/stoleurdata/Main.java
@@ -8,11 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
-    // test
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        var fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        var scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
