1709e10d89f3bb049712c896dcb5aa8f254e5a7f
thepigcat <benpospo@gmail.com>
2024-02-26 17:07:07 +0100

    add user saving/loading

--- src/main/java/com/pigrat/stoleurdata/DataManager.java
+++ src/main/java/com/pigrat/stoleurdata/DataManager.java
@@ -1,6 +1,7 @@
 package com.pigrat.stoleurdata;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class DataManager {
     public static final String PATH = "savedData.txt";
@@ -25,12 +26,21 @@ public class DataManager {
         }
     }
 
-    public static String load() {
+    public static ArrayList<String> load() {
         try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
-            return reader.readLine();
+            return new ArrayList<String>(reader.lines().toList());
         } catch (IOException e) {
             System.err.println("Error writing to the file: " + e.getMessage());
         }
         return null;
+    }
+
+    public static void saveUser(User toSave) {
+        save(toSave.name+"\n"+toSave.age+"\n"+toSave.gender);
+    }
+
+    public static User loadUser() {
+        var loaded = load();
+        return new User(loaded.get(0), Integer.parseInt(loaded.get(1)), loaded.get(2));
     }
 }
